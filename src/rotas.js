80f0cffb12cb281c10951f4b8233fa96cc7c71c3
roberto <robertosvalentim@gmail.com>
2023-02-02 14:35:37 -0300

    primeiro commit

---
+++ src/rotas.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+
+
+const rotas = express();
+
+//###### User ######
+//Add
+//Get
+//Login
+//Update
+//Delete
+
+//Add Favorites
+//Get Favorites
+//Get All Favorites
+//Delete Favorites
+
+//Add Comment
+//Get Comment
+//Delete Comment
